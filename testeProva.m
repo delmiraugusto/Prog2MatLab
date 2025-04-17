19daa7ae9ae0ec38166d03b56df23995067f3da0
delmir <delmir19.da@gmail.com>
2025-04-17 14:09:47 -0300

    Subindo teste

--- testeProva.m
+++ testeProva.m
@@ -5,9 +5,9 @@ clc;
 v1 = input('Digite o primeiro valor: ');
 v2 = input('Digite o segundo valor: ');
 
-maior = max(v1, v2)
-menor = min(v1, v2)
+maior = max(v1, v2);
+menor = min(v1, v2);
 
-resultado = maior / menor
+resultado = maior / menor;
 
-fprintf('%.1f', resultado);
+fprintf('%f\n', resultado);
